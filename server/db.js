13beccbac77ffd20614f08bda62f447508f58d8e
Surya Panduri <suryapanduri5121@gmail.com>
2025-08-19 23:08:15 +0530

    db.js file is updated for deployemnt.

--- server/db.js
+++ server/db.js
@@ -7,9 +7,12 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT // optional if 3306
+    port: process.env.DB_PORT, // optional if 3306
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 pool.getConnection((err,connection) => {
