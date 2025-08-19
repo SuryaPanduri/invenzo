a4ffb9056eb09bc63dc05a3c1def92faa6769c63
Surya Panduri <suryapanduri5121@gmail.com>
2025-08-19 22:34:34 +0530

    server file updated for deployment.

--- server/app.js
+++ server/app.js
@@ -23,11 +23,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/users', userRoutes);
 app.use('/api/assets', require('./routes/assets'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 // ✅ Start the server
-app.listen(3000, () => {
-  console.log('✅ Server listening at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server listening at http://localhost:${PORT}`);
 });
